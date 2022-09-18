bb4ecf6c8d8767aef227c955916f0523d821be02
to24toro <to24toro@gmail.com>
2022-09-18 17:36:32 +0900

    fix S3.tf

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -3,7 +3,7 @@
 # -----------------------------------
 resource "aws_s3_bucket" "terraform_test" {
   bucket = "test-bucket-20220918"
-  acl = "private"
+  acl    = "private"
   versioning {
     enabled = true
   }
