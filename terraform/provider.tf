7a92871ac2ac1ff3b4403f74cdd418be3c18ce62
to24toro <to24toro@gmail.com>
2022-09-21 18:01:14 +0900

    rename and set s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"
+  bucket = "terraform-state-bucket-bot-20220930"
   versioning {
     enabled = true
   }
