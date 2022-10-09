3809a930dce5e1b529f678ac3be6bba3502d9db0
to24toro <to24toro@gmail.com>
2022-10-09 20:25:42 +0900

    hash_key and range_key changed

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,23 +1,20 @@
 resource "aws_dynamodb_table" "purchase_history" {
-    name = "purchase_history"
-    billing_mode = "PROVISIONED"
-    read_capacity = 20
-    write_capacity = 20
-    hash_key = "Day"
-    range_key = "Price"
-    attribute {
-        name = "Item"
-        type = "S"
-    }
-    attribute {
-        name = "Day"
-        type = "N"
-    }
-    attribute {
-        name = "Price"
-        type = "N"
-    }
-    tags = {
-        Name = "example"
-    }
+  name           = "purchase_history"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "Date"
+  range_key      = "Item"
+
+  attribute {
+    name = "Date"
+    type = "N"
+  }
+  attribute {
+    name = "Item"
+    type = "N"
+  }
+  tags = {
+    Name = "example"
+  }
 }
