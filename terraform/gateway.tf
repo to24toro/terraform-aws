f9f297e7be00fcedffdab11f57c6d330f2ae32cf
to24toro <to24toro@gmail.com>
2022-10-23 17:50:25 +0900

    comment out eip and nat

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -6,8 +6,8 @@ resource "aws_internet_gateway" "internet_gateway_example" {
   }
 }
 
-#NAT Gateway
-resource "aws_nat_gateway" "nat_example" {
-  subnet_id     = aws_subnet.subnet_public_example_1a
-  allocation_id = aws_eip.eip_example.id
-}
+# #NAT Gateway
+# resource "aws_nat_gateway" "nat_example" {
+#   subnet_id     = aws_subnet.subnet_public_example_1a
+#   allocation_id = aws_eip.eip_example.id
+# }
