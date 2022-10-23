6c6772d1222b276cf72bbe2798e84b595f7cacba
to24toro <to24toro@gmail.com>
2022-10-23 17:39:11 +0900

    fmt

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,11 +1,8 @@
 # Public
 resource "aws_subnet" "subnet_public_example_1a" {
-  vpc_id = aws_vpc.vpc_example.id
-
+  vpc_id            = aws_vpc.vpc_example.id
   availability_zone = var.availability_zone
-
-  cidr_block = "10.0.1.0/24"
-
+  cidr_block        = "10.0.1.0/24"
   tags = {
     Name = "example"
   }
@@ -13,12 +10,9 @@ resource "aws_subnet" "subnet_public_example_1a" {
 
 # Private
 resource "aws_subnet" "subnet_private_example_1a" {
-  vpc_id = aws_vpc.vpc_example.id
-
+  vpc_id            = aws_vpc.vpc_example.id
   availability_zone = var.availability_zone
-
-  cidr_block = "10.0.10.0/24"
-
+  cidr_block        = "10.0.10.0/24"
   tags = {
     Name = "example"
   }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "vpc_example" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "example"
   }
