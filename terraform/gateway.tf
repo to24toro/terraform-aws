07d1b1497524238fcc2aaab29a50ba1eff83e069
to24toro <to24toro@gmail.com>
2022-10-23 18:09:29 +0900

    route_table

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -10,4 +10,7 @@ resource "aws_internet_gateway" "internet_gateway_example" {
 # resource "aws_nat_gateway" "nat_example" {
 #   subnet_id     = aws_subnet.subnet_public_example_1a
 #   allocation_id = aws_eip.eip_example.id
+#   tags = {
+#       Name = "example"
+#   }
 # }
